f63a5b185ec0c64e39f71a73f171bab940b45c1d
kukuminer <liranzh2000@gmail.com>
2023-04-01 11:50:01 -0400

    begin making prof section page

--- client/src/pages/section/professor/professor_section.js
+++ client/src/pages/section/professor/professor_section.js
@@ -1,6 +1,13 @@
+import getUser from "../../../getUser"
+
 const ProfessorSection = () => {
+    const id = getUser()
+    
     return (
-        <h2>Applicants:</h2>
+        <>
+            <h2>Applicants:</h2>
+
+        </>
     )
 }
 
