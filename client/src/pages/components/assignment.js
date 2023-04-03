cd56c82dcf857919626680bb29d2046039dc4f25
kukuminer <liranzh2000@gmail.com>
2023-04-03 00:31:14 -0400

    abstracted Assignment class
    next is sending post requests

--- client/src/pages/components/assignment.js
+++ client/src/pages/components/assignment.js
@@ -1,9 +1,53 @@
+import React from "react"
+import axios from "axios"
+
 class Assignment extends React.Component {
+    constructor(props) {
+        super(props)
+        if(!props.data.pref) props.data.pref = 0
+        if(!props.data.note) props.data.note = ''
+        this.state = {
+            key: props.rowKey,
+            data: props.data,
+            pref: props.data.pref,
+            note: props.data.note,
+        }
+        // console.log(props.data)
+    }
+
+    updateAssignment = () => {
+
+    }
+
+    doChangesExist = () => {
+        var prefChanged = (this.state.pref !== this.state.data.pref)
+        var noteChanged = (this.state.note !== this.state.data.note)
+        return prefChanged || noteChanged
+    }
 
     render() {
+        // There is a chance that the value order may not be maintained here!!!
+        var columnData = Object.values(this.state.data)
+        columnData[0] = columnData[0] + ' ' + columnData[1]
+        columnData.splice(1, 1)
+        columnData.splice(4, 2)
+        var tableCells = []
+        for (const val of columnData) {
+            tableCells.push(<td>{val}</td>)
+        }
+        const changesMade = this.doChangesExist()
         return (
-            <tr>
-                <td>Assignment row</td>
+            <tr key={this.state.key}>
+                {tableCells}
+                <td>
+                    <input type={'number'} value={this.state.pref} onChange={(event) => this.setState({pref: Number(event.target.value)})}/>
+                </td>
+                <td>
+                    <input type={'text'} value={this.state.note} onChange={(event) => this.setState({note: event.target.value})}/>
+                </td>
+                <td>
+                    <button onClick={this.updateAssignment} disabled={!changesMade}>Update</button>
+                </td>
             </tr>
         )
     }
--- client/src/pages/section/professor/professor_section.js
+++ client/src/pages/section/professor/professor_section.js
@@ -2,15 +2,16 @@ import getUser from "../../../getUser"
 import React from "react"
 import axios from "axios"
 import { useParams } from 'react-router-dom'
+import Assignment from "../../components/assignment"
 
 const ProfessorSection = () => {
     const id = getUser()
     const { course, letter } = useParams()
 
     const [tableData, setTableData] = React.useState(null)
-    const [pref, setPref] = React.useState(null)
-    const [note, setNote] = React.useState(null)
-    const [changesMade, setChangesMade] = React.useState(false)
+    // const [pref, setPref] = React.useState(null)
+    // const [note, setNote] = React.useState(null)
+    // const [changesMade, setChangesMade] = React.useState(false)
 
 
     React.useEffect(() => {
@@ -22,37 +23,37 @@ const ProfessorSection = () => {
             .then((res) => {
                 console.log('queried');
                 setTableData(res.data)
-                setPref(res.data.pref ? res.data.pref : 0)
-                setNote(res.data.note ? res.data.note : '')
+                // setPref(res.data.pref ? res.data.pref : 0)
+                // setNote(res.data.note ? res.data.note : '')
             })
     }, [course, letter, id])
 
 
-    React.useEffect(() => {
-        if (tableData) {
-            if (pref) setChangesMade(!(pref === tableData.pref))
-            if (note) setChangesMade(!(note === tableData.note))
-        }
-    }, [pref, note, tableData])
+    // React.useEffect(() => {
+    //     if (tableData) {
+    //         if (pref) setChangesMade(!(pref === tableData.pref))
+    //         if (note) setChangesMade(!(note === tableData.note))
+    //     }
+    // }, [pref, note, tableData])
 
-    const updateAssignment = () => {
-        console.log('updating!')
-        const url = '/api/professor/' + course + '/' + letter + '/' + id
-        const body = {
-            pref: pref,
-            note: note,
-        }
-        axios.post(url, body)
-            .then((res) => {
-                console.log('done!')
-                console.log(res.data)
-            })
-            .catch((error) => {
-                console.log('error posting pref/note: ', error)
-            })
-        // axios.post(url, {pref: 'bingbong'})
-        setChangesMade(false)
-    }
+    // const updateAssignment = () => {
+    //     console.log('updating!')
+    //     const url = '/api/professor/' + course + '/' + letter + '/' + id
+    //     const body = {
+    //         pref: pref,
+    //         note: note,
+    //     }
+    //     axios.post(url, body)
+    //         .then((res) => {
+    //             console.log('done!')
+    //             console.log(res.data)
+    //         })
+    //         .catch((error) => {
+    //             console.log('error posting pref/note: ', error)
+    //         })
+    //     // axios.post(url, {pref: 'bingbong'})
+    //     setChangesMade(false)
+    // }
 
     return (
         <>
@@ -71,15 +72,7 @@ const ProfessorSection = () => {
                     {
                         !tableData ? <tr><td>loading...</td></tr> : tableData.map((val, key) => {
                             return (
-                                <tr key={key}>
-                                    <td>{val.firstname} {val.lastname}</td>
-                                    <td>{val.grade}</td>
-                                    <td>{val.interest}</td>
-                                    <td>{val.qualification}</td>
-                                    <td><input type={"number"} value={pref} onChange={(event) => setPref(event.target.value)} /></td>
-                                    <td>{val.note}</td>
-                                    <td><button onClick={updateAssignment} disabled={!changesMade}>Update</button></td>
-                                </tr>
+                                <Assignment data={val} rowKey={key} key={key}/>
                             )
                         })
                     }
