92de6a70334122dcaa56040abee2acdb6e349e7b
kukuminer <liranzh2000@gmail.com>
2023-07-27 16:29:51 -0400

    pretty sure this commit is not needed

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -3,6 +3,7 @@ import React from "react";
 import axios from 'axios'
 import { Navigate } from "react-router-dom"
 import getUser from "../../getUser";
+import { FormControl, TextField, Button } from "@mui/material";
 
 const NO_USER_NAVIGATE = '/profile'
 const DEFAULT_NAVIGATE = '/dashboard'
@@ -11,17 +12,18 @@ const USER_URL = '/api/usertype/'
 const Login = (props) => {
     const [target, setTarget] = React.useState(props.target ? props.target : DEFAULT_NAVIGATE)
     const [ready, setReady] = React.useState(false)
+    const [formState, setFormState] = React.useState({})
 
     React.useEffect(() => {
         const url = USER_URL + getUser()
         axios.get(url)
             .then((res) => {
                 console.log(res.data)
-                if(!res.data.usertype) {
+                if (!res.data.usertype) {
                     setTarget(NO_USER_NAVIGATE)
                     setReady(true)
                 }
-                else{
+                else {
                     setReady(true)
                 }
             })
@@ -30,11 +32,61 @@ const Login = (props) => {
             })
     }, [])
 
+    function handleChange() {
+
+    }
+
+    function handleSubmit() {
+
+    }
 
     return (
         <>
-        hello
-            {ready ? <Navigate to={target} /> : <p>Redirecting...</p>}
+            <div className="profile">
+                <div className="main">
+                    <h1>
+                        TA Application System - Create User
+                    </h1>
+                    <form onSubmit={handleSubmit}>
+                        <FormControl margin="normal">
+                            <div sx={{ display: 'flex', flexDirection: "row", justifyContent: 'center' }}>
+                                <TextField
+                                    required
+                                    id="firstname"
+                                    value={formState.firstname}
+                                    error={!formState.firstname}
+                                    onChange={handleChange}
+                                    label="First Name"
+                                    margin="normal"
+                                />
+                                <TextField
+                                    required
+                                    id="lastname"
+                                    value={formState.lastname}
+                                    error={!formState.lastname}
+                                    onChange={handleChange}
+                                    label="Surname"
+                                    margin="normal"
+                                />
+                            </div>
+                            <TextField
+                                required
+                                id="email"
+                                value={formState.email}
+                                error={!formState.email}
+                                onChange={handleChange}
+                                label="Email"
+                                margin="normal"
+                                type="email"
+                            />
+                            <Button variant="contained" type="submit">Save</Button>
+
+                        </FormControl>
+                    </form>
+
+                    {ready ? <Navigate to={target} /> : <p>Redirecting...</p>}
+                </div>
+            </div>
         </>
     )
 }
