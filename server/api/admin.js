4553295a053e1b45c900f68e66412d23b0cedff9
kukuminer <liranzh2000@gmail.com>
2023-09-14 14:51:16 -0400

    remove extraneous auth
    its done in the middleware

--- server/api/admin.js
+++ server/api/admin.js
@@ -51,7 +51,6 @@ module.exports = function ({ app, db, pgp }) {
     app.post("/api/admin/rofr", (req, res) => {
         const userId = res.locals.userid
         const usertype = res.locals.usertype
-        console.log(usertype)
         // if (usertype !== 'admin') {
         //     console.log('unauthorized request!')
         //     res.status(403).send('Unauthorized!')
@@ -113,59 +112,58 @@ module.exports = function ({ app, db, pgp }) {
      */
     app.post("/api/admin/upsert", (req, res) => {
         const userId = res.locals.userid
-        db.any('SELECT usertype FROM users WHERE username=$1', userId)
-            .then((data) => {
-                if (!(data.length === 1 && data[0].usertype === 'admin')) {
-                    console.log('unauthorized request!')
-                    res.status(403).send('Unauthorized!')
-                    return
-                }
+        // db.any('SELECT usertype FROM users WHERE username=$1', userId)
+        //     .then((data) => {
+        //         if (!(data.length === 1 && data[0].usertype === 'admin')) {
+        //             console.log('unauthorized request!')
+        //             res.status(403).send('Unauthorized!')
+        //             return
+        //         }
 
-                const tableName = new pgp.helpers.TableName(req.body.tableName)
-                const rows = req.body.rows
-                const constr = req.body.constraints.split(',')
-                const cols = req.body.columns.split(',')
+        const tableName = new pgp.helpers.TableName(req.body.tableName)
+        const rows = req.body.rows
+        const constr = req.body.constraints.split(',')
+        const cols = req.body.columns.split(',')
 
-                db.tx(async t => {
-                    let arr = []
-                    for (const item of rows) {
-                        if (item.join(',').length > 0) {
-                            const colSet = new pgp.helpers.ColumnSet(cols, { table: tableName })
-                            var tbl = {}
-                            for (const idx in item) {
-                                tbl[cols[idx]] = item[idx]
-                            }
-
-
-                            const dbQuery = pgp.helpers.insert(tbl, colSet) +
-                                ' ON CONFLICT (' + constr + ') DO UPDATE SET ' +
-                                colSet.assignColumns({ from: 'EXCLUDED', skip: constr })
-
-                            arr.push(t.any(dbQuery)
-                                .then(data => {
-                                    return data
-                                })
-                                .catch(error => {
-                                    console.log('error upsert at row: ' + item, error)
-                                })
-                            )
-
-                        }
+        db.tx(async t => {
+            let arr = []
+            for (const item of rows) {
+                if (item.join(',').length > 0) {
+                    const colSet = new pgp.helpers.ColumnSet(cols, { table: tableName })
+                    var tbl = {}
+                    for (const idx in item) {
+                        tbl[cols[idx]] = item[idx]
                     }
-                    return t.batch(arr)
-                })
-                    .then(data => {
-                        res.status(200).send(data)
-                    })
-                    .catch(error => {
-                        console.log('error upsert:', error)
-                        res.status(500).send(error)
-                    })
+
+
+                    const dbQuery = pgp.helpers.insert(tbl, colSet) +
+                        ' ON CONFLICT (' + constr + ') DO UPDATE SET ' +
+                        colSet.assignColumns({ from: 'EXCLUDED', skip: constr })
+
+                    arr.push(t.any(dbQuery)
+                        .then(data => {
+                            return data
+                        })
+                        .catch(error => {
+                            console.log('error upsert at row: ' + item, error)
+                        })
+                    )
+
+                }
+            }
+            return t.batch(arr)
+        })
+            .then(data => {
+                res.status(200).send(data)
             })
-            .catch((error) => {
-                res.status(400).send(error)
+            .catch(error => {
+                console.log('error upsert:', error)
+                res.status(500).send(error)
             })
     })
-
+    //         .catch((error) => {
+    //             res.status(400).send(error)
+    //         })
+    // })
 
 }
