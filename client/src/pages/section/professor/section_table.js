1891a337774c038adb3c8bc273fe4ca28ca4bec9
kukuminer <liranzh2000@gmail.com>
2023-10-13 11:51:36 -0400

    remove unused

--- client/src/pages/section/professor/section_table.js
+++ client/src/pages/section/professor/section_table.js
@@ -1,67 +1,5 @@
 import * as React from 'react';
 import { DataGrid, GridCellModes } from '@mui/x-data-grid';
-// import {
-//     randomCreatedDate,
-//     randomTraderName,
-//     randomUpdatedDate,
-// } from '@mui/x-data-grid-generator';
-
-// const columns = [
-//     { field: 'name', headerName: 'Name', width: 180, editable: true },
-//     { field: 'age', headerName: 'Age', type: 'number', editable: true },
-//     {
-//         field: 'dateCreated',
-//         headerName: 'Date Created',
-//         type: 'date',
-//         width: 180,
-//         editable: true,
-//     },
-//     {
-//         field: 'lastLogin',
-//         headerName: 'Last Login',
-//         type: 'dateTime',
-//         width: 220,
-//         editable: true,
-//     },
-// ];
-
-// const rows = [
-//     {
-//         id: 1,
-//         name: randomTraderName(),
-//         age: 25,
-//         dateCreated: randomCreatedDate(),
-//         lastLogin: randomUpdatedDate(),
-//     },
-//     {
-//         id: 2,
-//         name: randomTraderName(),
-//         age: 36,
-//         dateCreated: randomCreatedDate(),
-//         lastLogin: randomUpdatedDate(),
-//     },
-//     {
-//         id: 3,
-//         name: randomTraderName(),
-//         age: 19,
-//         dateCreated: randomCreatedDate(),
-//         lastLogin: randomUpdatedDate(),
-//     },
-//     {
-//         id: 4,
-//         name: randomTraderName(),
-//         age: 28,
-//         dateCreated: randomCreatedDate(),
-//         lastLogin: randomUpdatedDate(),
-//     },
-//     {
-//         id: 5,
-//         name: randomTraderName(),
-//         age: 23,
-//         dateCreated: randomCreatedDate(),
-//         lastLogin: randomUpdatedDate(),
-//     },
-// ];
 
 export default function ProfSectionTable({rows, columns, loading}) {
     const [cellModesModel, setCellModesModel] = React.useState({});
@@ -120,7 +58,7 @@ export default function ProfSectionTable({rows, columns, loading}) {
                 onCellEditStop={() => {console.log("bing")}}
                 hideFooter
                 disableRowSelectionOnClick
-                
+
             />
         </div>
     );
