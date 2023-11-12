eb574ff96e7e2bce783a646a6f74a5996a4bf0bd
kukuminer <liranzh2000@gmail.com>
2023-11-12 11:38:50 -0500

    unused import

--- client/src/pages/application/student/student_application.js
+++ client/src/pages/application/student/student_application.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import axios from "axios"
 import HtmlTooltip from "../../components/tooltip"
 import { Button, Checkbox, FormControlLabel, FormGroup, TextField } from "@mui/material"
