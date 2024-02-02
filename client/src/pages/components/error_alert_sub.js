78d82a3379349879ef17e707ee7d09fbbb7fbacf
kukuminer <liranzh2000@gmail.com>
2024-02-02 10:07:40 -0500

    add alert using pubsub

---
+++ client/src/pages/components/error_alert_sub.js
@@ -0,0 +1,38 @@
+import { Alert, AlertTitle } from "@mui/material";
+import { useState } from "react";
+import { PubSub } from "pubsub-js";
+
+export const alertSubName = "requestAlert";
+
+export default function ErrorAlertSub() {
+  const [alert, setAlert] = useState({ title: "", msg: "" });
+
+  var alertFunc = (trigger, { title, msg }) => {
+    setAlert({ title: title, msg: msg });
+  };
+
+  var alertSub = PubSub.subscribe(alertSubName, alertFunc);
+
+  return (
+    <>
+      {(alert.title || alert.msg) && (
+        <Alert
+          severity="error"
+          variant="filled"
+          onClose={() => {
+            alertFunc("closed", { title: "", msg: "" });
+          }}
+          style={{
+            position: "fixed",
+            top: "5vh",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <AlertTitle>{alert.title}</AlertTitle>
+          {alert.msg}
+        </Alert>
+      )}
+    </>
+  );
+}
