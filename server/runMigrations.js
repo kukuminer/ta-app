326a367f58bad7e8c5179d3765e4718491ac39ec
kukuminer <liranzh2000@gmail.com>
2024-02-15 17:10:27 -0500

    now always runs all migrations
    next to make it check if migrations are present. this means making a table to store present ones and checking length of table with length of files

--- server/runMigrations.js
+++ server/runMigrations.js
@@ -1,7 +1,27 @@
+function readFile(e) {}
+
+const BASE_PATH = "./database/migrations/";
+
 module.exports = async function ({ db, pgp }) {
   console.log("running migrations");
+
   const fs = require("fs");
-  const res = await db.one("SELECT NOW()");
-  console.log(res);
+  const files = fs.readdirSync(BASE_PATH);
+  console.log(files);
+  for (item of files) {
+    // console.log(item);
+    const mig = fs.readFileSync(BASE_PATH + item, { encoding: "utf-8" });
+    db.tx(async (t) => {
+      try {
+        const res = await t.none(mig);
+      } catch (err) {
+        console.log("ERROR WITH MIGRATIONS:", e);
+        console.log("SKIPPED MIGRATION: ", item);
+      }
+    });
+
+    // console.log(mig);
+  }
+
   console.log("Done migrations!");
 };
