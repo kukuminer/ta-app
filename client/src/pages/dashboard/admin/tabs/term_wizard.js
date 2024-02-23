1e3745192439d72239ac636e840e4ec4d5e32c34
kukuminer <liranzh2000@gmail.com>
2024-02-23 15:58:27 -0500

    table data populates properly

--- client/src/pages/dashboard/admin/tabs/term_wizard.js
+++ client/src/pages/dashboard/admin/tabs/term_wizard.js
@@ -1,6 +1,10 @@
 import { TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import DatagridTable from "../../../components/datagrid/datagrid_table";
+import { wget } from "../../../requestWrapper";
+import { useNavigate } from "react-router-dom";
+
+const GET_URL = "/api/admin/terms";
 
 /** @type {GridColDef[]} */
 const columns = [
@@ -10,24 +14,46 @@ const columns = [
     width: 150,
     headerClassName: "section-table-header",
   },
+  {
+    field: "term",
+    headerName: "Term Name",
+    width: 150,
+    flex: 1,
+    headerClassName: "section-table-header",
+  },
+  {
+    field: "visible",
+    headerName: "Visible",
+    width: 150,
+    headerClassName: "section-table-header",
+  },
 ];
 
 const TermWizard = () => {
   const [termName, setTermName] = useState("");
-  const [tableRows, setTableRows] = useState();
+  const [tableRows, setTableRows] = useState([]);
+
+  const nav = useNavigate();
 
   useEffect(() => {
-    setTableRows([{ id: 3, term: "W23", visible: false }]);
-  }, [setTableRows]);
+    async function fetchData() {
+      const res = await wget(nav, GET_URL);
+      console.log(res);
+      setTableRows(res.data);
+    }
+    fetchData();
+  }, [setTableRows, nav]);
 
   return (
     <>
-      <p>Enter name of new term</p>
-      <TextField
-        id="termWizardInput"
-        value={termName}
-        onChange={(e) => setTermName(e.target.value)}
-      />
+      <div>
+        <p>Enter name of new term</p>
+        <TextField
+          id="termWizardInput"
+          value={termName}
+          onChange={(e) => setTermName(e.target.value)}
+        />
+      </div>
       <p>Existing terms:</p>
       <DatagridTable
         columns={columns}
@@ -36,6 +62,7 @@ const TermWizard = () => {
         onEditStop={null}
         processRowUpdate={null}
         rows={tableRows ?? []}
+        rowHeight={40}
       />
     </>
   );
