a85bb314b2643e5c370a51429aae3ec4b7414bd4
kukuminer <liranzh2000@gmail.com>
2024-04-28 23:00:21 -0400

    add handler to index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 // require("dotenv").config();
 import { config } from "dotenv";
 config();
+import AS from "express-async-handler";
+
 /**
  * NOTE: In most of the endpoints, "id" actually refers to the username
  */
@@ -35,16 +37,16 @@ app.use(urlencoded({ extended: true }));
 
 // require("./middleware/auth")({ app, db, pgp });
 import { auth } from "./middleware/auth.mjs";
-auth({ app, db, pgp });
+auth({ app, db, pgp, AS });
 // require("./api/applicant")({ app, db, pgp });
 import { applicant } from "./api/applicant.js";
-applicant({ app, db, pgp });
+applicant({ app, db, pgp, AS });
 // require("./api/instructor")({ app, db, pgp });
 import { instructor } from "./api/instructor.js";
-instructor({ app, db, pgp });
+instructor({ app, db, pgp, AS });
 // require("./api/admin")({ app, db, pgp });
 import { admin } from "./api/admin.js";
-admin({ app, db, pgp });
+admin({ app, db, pgp, AS });
 
 db.one("SELECT now()", [])
   .then((data) => {
@@ -57,22 +59,25 @@ db.one("SELECT now()", [])
 /**
  * Gets all user info for profile page
  */
-app.get("/api/user/:userid", (req, res) => {
-  const id = res.locals.userid;
-  const dbQuery = `
+app.get(
+  "/api/user/:userid",
+  AS(async (req, res) => {
+    const id = res.locals.userid;
+    const dbQuery = `
     SELECT firstname, lastname, email, usertype, username
     FROM users
     WHERE username = $1
     `;
-  db.any(dbQuery, [id])
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log("error fetching user info from db:", error);
-      res.status(500).send(error);
-    });
-});
+    db.any(dbQuery, [id])
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        console.log("error fetching user info from db:", error);
+        res.status(500).send(error);
+      });
+  })
+);
 
 /**
  * Posts user info to DB
@@ -84,50 +89,58 @@ app.get("/api/user/:userid", (req, res) => {
     email='liranz@yorku.ca', usertype='admin'
     WHERE users.username='kuku'
  */
-app.post("/api/user/update", (req, res) => {
-  const r = req.body.state;
-  r.username = res.locals.userid;
-  r.usertype = res.locals.usertype;
+app.post(
+  "/api/user/update",
+  AS(async (req, res) => {
+    const r = req.body.state;
+    r.username = res.locals.userid;
+    r.usertype = res.locals.usertype;
 
-  const colSet = new pgp.helpers.ColumnSet(
-    ["username", "firstname", "lastname", "email", "usertype"],
-    { table: "users" }
-  );
-  const postQuery =
-    pgp.helpers.insert(r, colSet) +
-    ' ON CONFLICT ("username") DO UPDATE SET ' +
-    colSet.assignColumns({ from: "EXCLUDED", skip: "username" }) +
-    " RETURNING usertype";
+    const colSet = new pgp.helpers.ColumnSet(
+      ["username", "firstname", "lastname", "email", "usertype"],
+      { table: "users" }
+    );
+    const postQuery =
+      pgp.helpers.insert(r, colSet) +
+      ' ON CONFLICT ("username") DO UPDATE SET ' +
+      colSet.assignColumns({ from: "EXCLUDED", skip: "username" }) +
+      " RETURNING usertype";
 
-  db.any(postQuery)
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).send(error);
-    });
-});
+    db.any(postQuery)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send(error);
+      });
+  })
+);
 
 /**
  * Gets usertype from userId
  */
-app.get("/api/userdata", (req, res) => {
-  const id = res.locals.userid;
-  // SELECT usertype FROM users WHERE id = $1
-  db.oneOrNone("SELECT username, usertype FROM users WHERE username = $1", [id])
-    .then((data) => {
-      if (data) {
-        res.json(data);
-      } else {
-        res.json({ usertype: null });
-      }
-    })
-    .catch((error) => {
-      console.log("error retrieving usertype from db on login");
-      res.status(500).send(error);
-    });
-});
+app.get(
+  "/api/userdata",
+  AS(async (req, res) => {
+    const id = res.locals.userid;
+    // SELECT usertype FROM users WHERE id = $1
+    db.oneOrNone("SELECT username, usertype FROM users WHERE username = $1", [
+      id,
+    ])
+      .then((data) => {
+        if (data) {
+          res.json(data);
+        } else {
+          res.json({ usertype: null });
+        }
+      })
+      .catch((error) => {
+        console.log("error retrieving usertype from db on login");
+        res.status(500).send(error);
+      });
+  })
+);
 
 /**
  * Gets public section info, insecure endpoint (no id check)
@@ -136,22 +149,25 @@ app.get("/api/userdata", (req, res) => {
  *  letter (A)
  *  term (W23)
  */
-app.get("/api/section/:sectionId", (req, res) => {
-  const sectionId = req.params.sectionId;
-  const dbQuery = `
+app.get(
+  "/api/section/:sectionId",
+  AS(async (req, res) => {
+    const sectionId = req.params.sectionId;
+    const dbQuery = `
         SELECT course.code AS course, letter, term FROM section 
         INNER JOIN course ON section.course=course.id
         WHERE section.id = $1
     `;
-  db.any(dbQuery, sectionId)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log("error retrieving section data from db:", sectionId);
-      res.status(400).send(error);
-    });
-});
+    db.any(dbQuery, sectionId)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        console.log("error retrieving section data from db:", sectionId);
+        res.status(400).send(error);
+      });
+  })
+);
 
 // /**
 //  * Public endpoint to get term name from term ID
