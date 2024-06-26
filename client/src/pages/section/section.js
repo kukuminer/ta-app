fcc95a96979f316ed7395be6988e0605a77b450c
kukuminer <liranzh2000@gmail.com>
2024-06-26 12:52:50 -0400

    prettify

--- client/src/pages/section/section.js
+++ client/src/pages/section/section.js
@@ -1,50 +1,52 @@
-import React from "react"
-import { useParams } from 'react-router-dom'
-import Header from "../header/header"
-import ProfessorSection from "./professor/professor_section"
-import "./section.css"
+import React from "react";
+import { useParams } from "react-router-dom";
+import Header from "../header/header";
+import ProfessorSection from "./professor/professor_section";
+import "./section.css";
 
 const components = {
-    'student': null,
-    'instructor': <ProfessorSection />,
-    'admin': null,
-}
+  student: null,
+  instructor: <ProfessorSection />,
+  admin: null,
+};
 
 const Section = () => {
-    const { sectionId } = useParams()
+  const { sectionId } = useParams();
 
-    const [userType, setUserType] = React.useState(null);
-    const [course, setCourse] = React.useState(null);
-    const [letter, setLetter] = React.useState(null);
+  const [userType, setUserType] = React.useState(null);
+  const [course, setCourse] = React.useState(null);
+  const [letter, setLetter] = React.useState(null);
 
-    React.useEffect(() => {
-        fetch('/api/userdata')
-            .then((res) => res.json())
-            .then((data) => {
-                setUserType(data.usertype)
-            })
-    }, [])
+  React.useEffect(() => {
+    fetch("/api/userdata")
+      .then((res) => res.json())
+      .then((data) => {
+        setUserType(data.usertype);
+      });
+  }, []);
 
-    React.useEffect(() => {
-        fetch('/api/section/' + sectionId)
-            .then((res) => res.json())
-            .then((data) => {
-                setCourse(data[0].course)
-                setLetter(data[0].letter)
-            })
-    }, [sectionId])
+  React.useEffect(() => {
+    fetch("/api/section/" + sectionId)
+      .then((res) => res.json())
+      .then((data) => {
+        setCourse(data[0].course);
+        setLetter(data[0].letter);
+      });
+  }, [sectionId]);
 
-    return (
-        <>
-            <Header />
-            <div className="section">
-                <div className="main">
-                    <h1>SECTION {course ?? 'Loading..'} {letter}</h1>
-                    {components[userType]}
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="section">
+        <div className="main">
+          <h1>
+            SECTION {course ?? "Loading.."} {letter}
+          </h1>
+          {components[userType]}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Section
+export default Section;
