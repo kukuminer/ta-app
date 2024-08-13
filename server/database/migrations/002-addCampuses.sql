d06e0930227152ad258085f229e27664e8895919
kukuminer <liranzh2000@gmail.com>
2024-08-13 17:22:40 -0400

    update migrations

--- server/database/migrations/002-addCampuses.sql
+++ server/database/migrations/002-addCampuses.sql
@@ -1,7 +1,11 @@
 -- transaction not needed here because it runs as a tx when loading
 DROP TYPE IF EXISTS campus;
 
-CREATE TYPE campus AS ENUM ('Keele', 'Glendon', 'Markham');
+DO $$ BEGIN
+    CREATE TYPE campus AS ENUM ('Keele', 'Glendon', 'Markham');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
 
 ALTER TABLE section ADD COLUMN campus campus NOT NULL DEFAULT 'Keele';
 
--- server/database/migrations/004-addSeniority.sql
+++ server/database/migrations/004-addSeniority.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS unit2seniority (
+CREATE TABLE IF NOT EXISTS seniority (
     id SERIAL NOT NULL,
     employeeid varchar(9) NOT NULL,
     seniority float NOT NULL,
